5599f11f8739170eef962e5592b3c48e28e0151c
KaranKathur06 <karan.business.acc0461@gmail.com>
2026-07-13 20:33:46 +0530

    Remove old Prisma schema and temporary check script

--- tmp-check-prisma.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const p = new PrismaClient();
-console.log('keys', Object.keys(p).filter(k => /ingest|job/i.test(k)).sort());
-console.log('has dataIngestionJob', Object.prototype.hasOwnProperty.call(p, 'dataIngestionJob'));
-console.log('has DataIngestionJob', Object.prototype.hasOwnProperty.call(p, 'DataIngestionJob'));
-p.$disconnect();
